8167aa81002c4412ec49103b58ad33b63a5af908
Trong Huy <huytrongk8a@gmail.com>
2025-06-26 13:25:08 +0700

    Initial commit

---
+++ src/main/java/org/example/ktra/KTraApplication.java
@@ -0,0 +1,13 @@
+package org.example.ktra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KTraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KTraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ktra/KTraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ktra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KTraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
